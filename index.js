d0add77c1de1faeacfc2c2cbe579c398fa5567bf
hiqbal <hiqbal@fetchsky.com>
2018-01-17 17:27:17 +0500

    stacksize

--- index.js
+++ index.js
@@ -1,56 +1,65 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 // subscribed components update functions
-const subscribedComponents = []
-
+const subscribedComponents = [];
+let stackSize = 1;
 
 function getCurrentRouteKey(navigationState) {
-  if (!navigationState) return null
-  const route = navigationState.routes[navigationState.index]
-  if (route.routes) return getCurrentRouteKey(route)
-  return route.key
+  if (!navigationState) return null;
+  const route = navigationState.routes[navigationState.index];
+  if (route.routes) return getCurrentRouteKey(route);
+  return route.key;
 }
 
 function updateFocus(currentState) {
-  const currentRouteKey = getCurrentRouteKey(currentState)
-  subscribedComponents.forEach((f) => f(currentRouteKey))
+  const currentRouteKey = getCurrentRouteKey(currentState);
+  subscribedComponents.forEach(f => f(currentRouteKey));
+  if (currentState && currentState.routes) {
+    stackSize = currentState.routes.length;
+  }
 }
 
+function getStackSize() {
+  return stackSize;
+}
 
-function _bind(WrappedComponent, isInitialRoute) {
+function withNavigationFocus(WrappedComponent) {
   class WithNavigationFocus extends React.Component {
-
     static propTypes = {
       navigation: PropTypes.object.isRequired,
-    }
+    };
 
-    static navigationOptions = (props) => {
+    static navigationOptions = props => {
       if (typeof WrappedComponent.navigationOptions === 'function') {
-        return WrappedComponent.navigationOptions(props)
+        return WrappedComponent.navigationOptions(props);
       }
-      return { ...WrappedComponent.navigationOptions }
-    }
+      return { ...WrappedComponent.navigationOptions };
+    };
 
-    isFocused = !!isInitialRoute
-    state = {
-      isFocused: !!isInitialRoute
+    constructor(props) {
+      super(props);
+      this.state = {
+        isFocused: true,
+        focusedRouteKey: props.navigation.state.key,
+      };
+      this.isFocused = true;
     }
 
     componentDidMount() {
-      subscribedComponents.push(this._handleNavigationChange)
+      subscribedComponents.push(this._handleNavigationChange);
     }
 
     componentWillUnmount() {
       for (var i = 0; i < subscribedComponents.length; i++) {
         if (subscribedComponents[i] === this._handleNavigationChange) {
-          subscribedComponents.splice(i, 1)
-          break
+          subscribedComponents.splice(i, 1);
+          break;
         }
       }
     }
 
-    _handleNavigationChange = (routeKey) => {
+    _handleNavigationChange = routeKey => {
       // update state only when isFocused changes
       const currentScreenKey = this.props.navigation.state.key;
 
@@ -61,12 +70,12 @@ function _bind(WrappedComponent, isInitialRoute) {
       // immediately reflects any changes.
       if (this.isFocused !== (currentScreenKey === routeKey)) {
         this.setState({
-            isFocused: !this.isFocused,
-            focusedRouteKey: routeKey,
-        })
-        this.isFocused = !this.isFocused
+          isFocused: !this.isFocused,
+          focusedRouteKey: routeKey,
+        });
+        this.isFocused = !this.isFocused;
       }
-    }
+    };
 
     render() {
       return (
@@ -75,22 +84,13 @@ function _bind(WrappedComponent, isInitialRoute) {
           focusedRouteKey={this.state.focusedRouteKey}
           {...this.props}
         />
-      )
+      );
     }
   }
+  WithNavigationFocus.displayName =
+    WrappedComponent.displayName || WrappedComponent.name || 'Component';
 
-  WithNavigationFocus.displayName = WrappedComponent.displayName || WrappedComponent.name || 'Component'
-
-  return WithNavigationFocus
+  return WithNavigationFocus;
 }
 
-function withNavigationFocus(WrappedComponent, isInitialRoute) {
-  if (typeof WrappedComponent === 'function') { // standard HOC
-    return _bind(WrappedComponent, isInitialRoute)
-  } else {// ES7 decorator
-    isInitialRoute = WrappedComponent
-    return (WrappedComponent) => _bind(WrappedComponent, isInitialRoute)
-  }
-}
-
-export { getCurrentRouteKey, withNavigationFocus, updateFocus }
+export { getCurrentRouteKey, withNavigationFocus, updateFocus, getStackSize };
